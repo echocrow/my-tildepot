451a117a9f50007e4e3ea281774e5a9bf87a03de
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-03-28 23:19:44 +0100

    update tilde lib fns

--- bundles/icloud.sh
+++ bundles/icloud.sh
@@ -7,8 +7,8 @@ function INSTALL() {
   [ ! -e ~/Docs ] && ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents ~/Docs && created=1
   [ ! -e ~/Backlog ] && ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Backlog ~/Backlog && created=1
   if [[ -n $created ]]; then
-    TILDE::success "Created iCloud symlinks created."
+    tilde::success "Created iCloud symlinks created."
   else
-    TILDE::warning "Skipped iCloud symlinks; they already exist."
+    tilde::warning "Skipped iCloud symlinks; they already exist."
   fi
 }
