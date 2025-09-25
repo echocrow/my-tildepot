5970ebe22644b33d44b2350c83713cbe2980eb36
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-09-25 13:06:52 +0200

    simplify hassio ping script

--- state/files/home/scripts/bash/ping_hassio.sh
+++ state/files/home/scripts/bash/ping_hassio.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 
-# date >> /Users/echocrow/Desktop/cron-log.txt
-# date +%s >> ~/Desktop/cron-log.txt
-# date >> ~/Desktop/cron-log.txt
-# echo "$(date)" '|' "$(pmset -g systemstate | sed 's/[ \t]*$//' | tr '\n' '|' | sed 's/|$//;s/|/ | /')" >> ~/Desktop/cron-log.txt
-
-# curl -v 'http://homeassistant.local:8123/api/webhook/MiniTytoOnline'
-
-# echo -n "$(date)" >> ~/Desktop/cron-log.txt
-
 powerstate="$(pmset -g systemstate)"
-# posted='OMIT'
-if [[ 
-	"$powerstate" =~ Current\ System\ Capabilities\ are:[a-zA-Z\ ]*\ Graphics &&
-	"$powerstate" =~ Current\ System\ Capabilities\ are:[a-zA-Z\ ]*\ Network &&
-	"$powerstate" =~ Current\ Power\ State:\ 4 &&
-	! "$powerstate" =~ Desired\ State:\ 2 ]] \
-	; then
-	curl -v 'http://homeassistant.local:8123/api/webhook/MiniTytoOnline'
-	# posted='POST'
-	# else
-	# echo "BAD"
+if [[
+	$powerstate == *'Current System Capabilities are:'*' Graphics'* &&
+	$powerstate == *'Current System Capabilities are:'*' Network'* &&
+	$powerstate == *'Current Power State: 4'* &&
+	$powerstate != *'Desired State: 2'*
+]]; then
+	curl 'http://homeassistant.local:8123/api/webhook/MiniTytoOnline'
 fi
-exit 0
-
-# echo " | ${posted} | $(echo "$powerstate" | sed 's/[ \t]*$//' | tr '\n' '|' | sed 's/|$//;s/|/ | /')" >> ~/Desktop/cron-log.txt
